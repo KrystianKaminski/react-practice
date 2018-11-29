8a0bf275b08f5b8e178c2d925bef28d6e86a2377
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-29 21:47:57 +0100

    Quick refresh ES6 - modules

--- main.js
+++ main.js
@@ -1,41 +1,16 @@
-// CLASSES
+// MODULES
 
-class Person {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-    }
+// file 1 (file1.js)
 
-    greet() {
-        return `Hello, my name is ${this.name} and I am ${this.age}`
-    }
-}
+export const name = 'Jeff'
+export const nums = [1, 2, 3]
+export default Person
 
-const person1 = new Person('John', 33)
-const person2 = new Person('Sarah', 28)
 
-// console.log(person1.name)
-// console.log(person2.name)
-// console.log(person1.age)
-// console.log(person2.age)
+// file 2 (file2.js)
 
-// console.log(person1.greet())
-// console.log(person2.greet())
-
-// SUBCLASSES
-
-class Customer extends Person {
-    constructor(name, age, balance) {
-        super(name, age)
-        this.balance = balance
-    }
-
-    info() {
-        return `${this.name} owes ${this.balance}.00`
-    }
-}
-
-const customer1 = new Customer('Kevin', 32, 300)
-
-console.log(customer1.name)
-console.log(customer1.info())
+import {
+    name,
+    nums
+} from './file1'
+import Person from './file1'
