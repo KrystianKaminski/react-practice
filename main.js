200d4e3e1b73077edcd385628936a2b733e39c81
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-29 21:30:39 +0100

    Quick refresh ES6 - spread operator

--- main.js
+++ main.js
@@ -1,60 +1,19 @@
-// CONST & LET
+// SPREAD
 
-let name = 'John'
-let test;
+const arr = [1, 2, 3]
+const arr2 = [...arr, 4]
+const arr3 = [...arr.filter(num => num !== 2)]
 
-name = 'Jack'
+console.log(arr3)
 
-const person = {
-    name: 'John',
-    age: 33
+const person1 = {
+    name: 'Krystian',
+    age: 36
 }
 
-person.name = 'Jack'
+const person2 = {
+    ...person1,
+    email: 'brad@gmail.com'
+}
 
-
-const nums = [1, 2, 3, 4]
-nums.push(5)
-
-
-// ARROW FUNCTIONS
-
-// function sayHello() {
-//     console.log('Hello')
-// }
-
-const sayHello = name => console.log(`Hello ${name}`)
-
-
-// sayHello('Krystian')
-
-const fruits = ['Apples', 'Oranges', 'Grapes']
-
-// FOR EACH
-
-// fruits.forEach(fruit => console.log(fruit))
-
-// MAP
-
-const singleFruit = fruits.map((fruit) => fruit.slice(0, -1).toUpperCase())
-
-// console.log(singleFruit)
-
-// FILTER
-
-const people = [{
-        id: 1,
-        name: 'Karen'
-    },
-    {
-        id: 2,
-        name: 'Bob'
-    },
-    {
-        id: 3,
-        name: 'Sharon'
-    }
-]
-
-const people2 = people.filter(person => person.id != 2)
-console.log(people2)
+console.log(person2)
