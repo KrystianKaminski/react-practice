149f1f3fefef29f67db6ce5d9a94353b94519fd2
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-29 20:36:35 +0100

    Quick refresh ES6 - const and let

---
+++ main.js
@@ -0,0 +1,19 @@
+// CONST & LET
+
+let name = 'John'
+let test;
+
+name = 'Jack'
+
+const person = {
+    name: 'John',
+    age: 33
+}
+
+person.name = 'Jack'
+
+
+const nums = [1, 2, 3, 4]
+nums.push(5)
+
+console.log(nums)
