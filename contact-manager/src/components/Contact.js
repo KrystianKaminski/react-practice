ce8cf194050581b590b6e1fcbe7b6bbbb4dec48d
KrystianKaminski <krystiankaminski07@gmail.com>
2018-12-01 18:50:48 +0100

    Changing state - conditional rendering

--- contact-manager/src/components/Contact.js
+++ contact-manager/src/components/Contact.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Contact extends Component {
-    state = {}
-
-    onShowClick = (e) => {
-        console.log(e.target)
+    state = {
+        showContactInfo: false
     }
 
     render() {
 
         const { name, email, phone } = this.props.contact
+        const { showContactInfo } = this.state
         return (
             <div className="card card-body mb-3">
                 <h4>{name}
                     <i
                         className="fas fa-sort-down"
-                        onClick={this.onShowClick}
+                        onClick={() => this.setState({
+                            showContactInfo: !this.state.showContactInfo
+                        })}
                     />
                 </h4>
-                <ul className="list-group">
-                    <li className="list-group-item">Email:{email}</li>
-                    <li className="list-group-item">Phone:{phone}</li>
-                </ul>
+                {showContactInfo ? (
+                    <ul className="list-group">
+                        <li className="list-group-item">Email:{email}</li>
+                        <li className="list-group-item">Phone:{phone}</li>
+                    </ul>
+                )
+                    :
+                    null}
             </div>
         )
     }
