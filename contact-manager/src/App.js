18e2676cf6455826ab94a0c49dc44c382dd3a43a
KrystianKaminski <krystiankaminski07@gmail.com>
2018-12-01 17:19:18 +0100

    JSX expression

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'Larry'
+
     return (
       <div className="App">
         <h1>The App Component</h1>
+        <h4>Hello {name}</h4>
       </div>
     );
   }
