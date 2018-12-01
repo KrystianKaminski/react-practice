d699e0aab7c3e481c778bf19e5ada8d9bdb178dd
KrystianKaminski <krystiankaminski07@gmail.com>
2018-12-01 18:22:21 +0100

    Created state - recomposed components

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact'
 import Header from './components/Header'
+import Contacts from './components/Contacts'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
@@ -14,16 +14,7 @@ class App extends Component {
           branding='Contact Manager'
         />
         <div className="container">
-          <Contact
-            name="John Doe"
-            email="jdoe@gmail.com"
-            phone="555-555-5555"
-          />
-          <Contact
-            name="Karen Smith"
-            email="karen@gmail.com"
-            phone="333-333-3333"
-          />
+          <Contacts />
         </div>
       </div>
     );
