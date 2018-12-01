ecd6b35137e108cb1dffaf64d2b2cd11c98dd243
KrystianKaminski <krystiankaminski07@gmail.com>
2018-12-01 18:07:15 +0100

    Bootstrap installed

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Contact from './components/Contact'
 import Header from './components/Header'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 class App extends Component {
@@ -12,16 +13,18 @@ class App extends Component {
         <Header
           branding='Contact Manager'
         />
-        <Contact
-          name="John Doe"
-          email="jdoe@gmail.com"
-          phone="555-555-5555"
-        />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="333-333-3333"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="333-333-3333"
+          />
+        </div>
       </div>
     );
   }
