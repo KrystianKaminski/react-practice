c218ab91aefe0d6979b953ca3e9e707f7bd22530
KrystianKaminski <krystiankaminski07@gmail.com>
2018-12-03 20:07:47 +0100

    Updates

---
+++ precache-manifest.ae258232637fc47d473a94f64163c0c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "0f6937f47688564e5508",
+    "url": "/contactmanager/static/js/main.0f6937f4.chunk.js"
+  },
+  {
+    "revision": "a22a5ae71ad5ab366fe1",
+    "url": "/contactmanager/static/js/1.a22a5ae7.chunk.js"
+  },
+  {
+    "revision": "0f6937f47688564e5508",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "a22a5ae71ad5ab366fe1",
+    "url": "/contactmanager/static/css/1.5bfbc2e3.chunk.css"
+  },
+  {
+    "revision": "ba0cb61313f5ab4e08b6bb047c1e0eba",
+    "url": "/contactmanager/index.html"
+  }
+];
