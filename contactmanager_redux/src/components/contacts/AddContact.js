ee8f96b1b4a8043867fb3fbdcdbdae52bfa1682d
KrystianKaminski <krystiankaminski07@gmail.com>
2018-12-05 20:17:24 +0100

    Delete and add contact actions

--- contactmanager_redux/src/components/contacts/AddContact.js
+++ contactmanager_redux/src/components/contacts/AddContact.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import TextInputGroup from '../layout/TextInputGroup';
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { addContact } from '../../actions/contactActions'
+import uuid from 'uuid'
+
 
 class AddContact extends Component {
   state = {
@@ -31,10 +36,13 @@ class AddContact extends Component {
     }
 
     const newContact = {
+      id: uuid(),
       name,
       email,
-      phone
+      phone,
     };
+
+    this.props.addContact(newContact)
 
     //// SUBMIT CONTACT ////
 
@@ -96,4 +104,8 @@ class AddContact extends Component {
   }
 }
 
-export default AddContact;
+AddContact.propTypes = {
+  addContact: PropTypes.func.isRequired
+}
+
+export default connect(null, { addContact })(AddContact);
