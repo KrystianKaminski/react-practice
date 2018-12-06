2dbd8ce6b1203fb3fe0acef28464f901aac17fbb
KrystianKaminski <krystiankaminski07@gmail.com>
2018-12-06 19:30:13 +0100

    PUT request and dispatch

--- contactmanager_redux/src/components/contacts/EditContact.js
+++ contactmanager_redux/src/components/contacts/EditContact.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TextInputGroup from '../layout/TextInputGroup';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { getContact } from '../../actions/contactActions'
+import { getContact, updateContact } from '../../actions/contactActions'
 
 class EditContact extends Component {
   state = {
@@ -47,14 +47,17 @@ class EditContact extends Component {
       return;
     }
 
+
+    const { id } = this.props.match.params;
+
     const updContact = {
+      id,
       name,
       email,
       phone
     };
 
-    const { id } = this.props.match.params;
-
+    this.props.updateContact(updContact)
     //// UPDATE CONTACT ////
 
     // Clear State
@@ -124,4 +127,4 @@ const mapStateToProps = state => ({
   contact: state.contact.contact
 })
 
-export default connect(mapStateToProps, { getContact })(EditContact);
+export default connect(mapStateToProps, { getContact, updateContact })(EditContact);
