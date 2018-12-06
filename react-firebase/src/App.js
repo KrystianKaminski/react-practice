a53db066f833f06a4999df94f7c630ed707dc4a3
KrystianKaminski <krystiankaminski07@gmail.com>
2018-12-06 20:46:29 +0100

    Dashboard, sidebar and clients component

--- react-firebase/src/App.js
+++ react-firebase/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import AppNavbar from './components/layout/AppNavbar'
+import Dashboard from './components/layout/Dashboard'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
@@ -12,7 +13,14 @@ class App extends Component {
       <Router>
         <div className="App">
           <AppNavbar />
-          <h1>Hello</h1>
+          <div
+            className="container"
+          >
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+            </Switch>
+
+          </div>
         </div>
       </Router>
     );
